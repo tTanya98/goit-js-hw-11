039faf2ac799c89822ad401cfcb5ece839b0066f
tTanya98 <chichkinat98@gmail.com>
2023-04-02 16:57:25 +0300

    update index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formSearch = document.querySelector('.search-form');
+const galleryCont = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+
+let isShown = 0;
+// const newApiService = new NewApiService();
