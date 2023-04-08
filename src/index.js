446997cfccb5a28de4deeaeacce499713520a7c9
tTanya98 <chichkinat98@gmail.com>
2023-04-08 12:00:45 +0300

    updt

--- src/index.js
+++ src/index.js
@@ -197,155 +197,96 @@
 
 //   galleryCont.insertAdjacentHTML('beforeend', markup);
 // }
+
 import './sass/index.scss';
-import axios from 'axios';
+import { fetchImages } from './js/fetch-images';
+import { renderGallery } from './js/render-gallery';
+import { onScroll, onToTopBtn } from './js/scroll';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// пошук елементів документа
-const refs = {
-  form: document.querySelector('.search-form'),
-  input: document.querySelector('input'),
-  gallery: document.querySelector('.gallery'),
-  btnLoadMore: document.querySelector('.load-more'),
-};
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.btn-load-more');
+let query = '';
+let page = 1;
+let simpleLightBox;
+const perPage = 40;
 
-let page = 1; // початкове значення параметра page повинно бути 1 сторніка
+searchForm.addEventListener('submit', onSearchForm);
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
 
-refs.btnLoadMore.style.display = 'none'; // ховаємо кнопку
-refs.form.addEventListener('submit', onSearch); // слухач події на submit
-refs.btnLoadMore.addEventListener('click', onBtnLoadMore); // слухач події клік по кнопці loadMore
+onScroll();
+onToTopBtn();
 
-// обробка події на submit
-function onSearch(evt) {
-  evt.preventDefault(); // відміна перезавантаження сторінки
-
+function onSearchForm(e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0 });
   page = 1;
-  refs.gallery.innerHTML = ''; // очищення попереднього вмісту галереї
+  query = e.currentTarget.searchQuery.value.trim();
+  gallery.innerHTML = '';
+  loadMoreBtn.classList.add('is-hidden');
 
-  const name = refs.input.value.trim(); // обрізання пробілів до і після слова
-
-  // якщо слово пошука НЕ пуста строка то:
-  if (name !== '') {
-    pixabay(name); // отримати зображення
-
-  } else {
-    refs.btnLoadMore.style.display = 'none';
-
-    // вивести повідомлення про те, що НЕ знайдено жодного зображення
-    return Notiflix.Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  }
-}
-
-// дії кнопки LoadMore
-function onBtnLoadMore() {
-  const name = refs.input.value.trim();
-  page += 1; // додаємо +1 сторінку яка має +40 картинок
-  pixabay(name, page); // завантаження зображень
-}
-
-// отримання зображень
-async function pixabay(name, page) {
-  const API_URL = 'https://pixabay.com/api/';
-
-  // параметри запиту на бекенд
-  const options = {
-    params: {
-      key: '34997553-a7b2d36a26db158f740bb35fd', // мій персональний ключ з pixabay
-      q: name,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
-      page: page,
-      per_page: 40,
-    },
-  };
-
-  try {
-    // отримання відповіді-результату від бекенду
-    const response = await axios.get(API_URL, options);
-
-    // сповіщення notiflix
-    notification(
-      response.data.hits.length, // довжина всіх знайдених зображень
-      response.data.total // отримання кількості
-    );
-
-    createMarkup(response.data); // рендер розмітки на сторінку
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-// рендер розмітки на сторінку
-function createMarkup(arr) {
-  const markup = arr.hits
-    .map(
-      item =>
-        `<a class="photo-link" href="${item.largeImageURL}">
-            <div class="photo-card">
-            <div class="photo">
-            <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy"/>
-            </div>
-                    <div class="info">
-                        <p class="info-item">
-                            <b>Likes</b>
-                            ${item.likes}
-                        </p>
-                        <p class="info-item">
-                            <b>Views</b>
-                            ${item.views}
-                        </p>
-                        <p class="info-item">
-                            <b>Comments</b>
-                            ${item.comments}
-                        </p>
-                        <p class="info-item">
-                            <b>Downloads</b>
-                            ${item.downloads}
-                        </p>
-                    </div>
-            </div>
-        </a>`
-    )
-    .join(''); // сполучення рядків всіх об'єктів (всіх картинок)
-  refs.gallery.insertAdjacentHTML('beforeend', markup); // вставлення розмітки на сторінку
-  simpleLightBox.refresh(); // оновлення екземпляру lightbox
-}
-
-// екземпляр модального вікна слайдера-зображень
-const simpleLightBox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt', // опис
-  captionDelay: 250, // затримка 250 мілісекунд
-});
-
-// всі сповіщення notiflix
-function notification(length, totalHits) {
-  if (length === 0) {
-
-      // вивести повідомлення про те, що НЕ знайдено жодного зображення
-    Notiflix.Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
+  if (query === '') {
+    alertNoEmptySearch();
     return;
   }
 
-  if (page === 1) {
-    refs.btnLoadMore.style.display = 'flex'; // показуємо кнопку loadMore
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      if (data.totalHits === 0) {
+        alertNoImagesFound();
+      } else {
+        renderGallery(data.hits);
+        simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+        alertImagesFound(data);
 
-    // вивести повідомлення про кількість знайдених зобрежнь
-    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-  }
+        if (data.totalHits > perPage) {
+          loadMoreBtn.classList.remove('is-hidden');
+        }
+      }
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      searchForm.reset();
+    });
+}
 
-  if (length < 40) {
-    refs.btnLoadMore.style.display = 'none'; // ховаємо кнопку loadMore
+function onLoadMoreBtn() {
+  page += 1;
+  simpleLightBox.destroy();
 
-      // вивести інфо-повідомлення про те, що більше вже немає зображень
-    Notiflix.Notify.info(
-      "We're sorry, but you've reached the end of search results."
-    );
-  }
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      renderGallery(data.hits);
+      simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+
+      const totalPages = Math.ceil(data.totalHits / perPage);
+
+      if (page > totalPages) {
+        loadMoreBtn.classList.add('is-hidden');
+        alertEndOfSearch();
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function alertImagesFound(data) {
+  Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+}
+
+function alertNoEmptySearch() {
+  Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.');
+}
+
+function alertNoImagesFound() {
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.',
+  );
+}
+
+function alertEndOfSearch() {
+  Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
 }
--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -1,7 +1,7 @@
 // import axios from 'axios';
 
-// const BASE_URL = 'https://pixabay.com/api/';
-// // const API_KEY = '34997553-a7b2d36a26db158f740bb35fd';
+// const API_URL = 'https://pixabay.com/api/';
+// const API_KEY = '34997553-a7b2d36a26db158f740bb35fd';
 
 // export default class NewsApiImages {
 //   constructor() {
@@ -46,3 +46,16 @@
 //     return this.page === Math.ceil(this.totalPages / this.per_page);
 //   }
 // }
+
+import axios from 'axios';
+export { fetchImages };
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const KEY = '34997553-a7b2d36a26db158f740bb35fd';
+
+async function fetchImages(query, page, perPage) {
+  const response = await axios.get(
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+  );
+  return response;
+}
